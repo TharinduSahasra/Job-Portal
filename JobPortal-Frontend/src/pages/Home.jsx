b81d2176e186ebe6e9df1b931e707559186d31d0
TharinduSahasra <sahasrasenevirathne@gmail.com>
2024-11-30 19:05:54 +0530

    Design the landing page

---
+++ JobPortal-Frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import HeroSection from "../components/HeroSection";
+import ButtonsSection from "../components/ButtonsSection";
+import Footer from "../components/footer/Index";
+
+const Home = () => {
+    return (
+      <div className="w-screen h-screen flex flex-col justify-between bg-slate-950">
+        <div className="pt-40 px-32">
+          <HeroSection />
+          <ButtonsSection />
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  export default Home;
